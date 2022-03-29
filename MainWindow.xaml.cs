332a92beaeddda95b4549aff2d5ec0fb03079f2e
memetrollsXD <memetrollsxd@gmail.com>
2022-03-29 21:53:07 +0200

    More design

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Runtime.InteropServices;
+using System;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Chef
 {
@@ -23,6 +13,28 @@ namespace Chef
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void toTaskbar(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void fromTaskbar(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+        }
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
